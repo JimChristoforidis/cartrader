954d4d95ce6fe24dc5c11ca99a9909f0c3cc6600
JimChristoforidis <146664023+JimChristoforidis@users.noreply.github.com>
2023-11-19 11:03:18 +0200

    Add files via upload

---
+++ nuxt.config.ts
@@ -0,0 +1,5 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+ modules: ["@nuxtjs/tailwindcss", "@nuxtjs/supabase", "@nuxt/image-edge", "@nuxt/image","@vueuse/nuxt", ]
+ 
+})
